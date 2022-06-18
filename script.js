396d71b5530dc2087d585157504388b42793894e
Alexandra Pinenkova <sashapinenkova@gmail.com>
2022-06-18 19:42:44 +0200

    18.06.2022

--- script.js
+++ script.js
@@ -1,15 +1,41 @@
-let SashaTest = document.getElementById("edit-button");
-let MopsTest = document.querySelector(".popup");
+const popupAll = document.querySelectorAll('.popup');
+const popupProfile = document.querySelector('.popup_profile-edit');
+//
+const editButton = document.querySelector('.profile__edit-button');
+const placePopup = document.querySelector('.profile__add-button');
+//
 
-SashaTest.addEventListener("click", function() {
-    MopsTest.classList.add("popup_opened");
-  });
+function popupOpen (element) {
+    element.classList.add('popup_opened');
+}
 
-let CatTest = document.getElementById("close-button");
+function popupClose (element) {
+    element.classList.remove('popup_opened');
+}
 
-CatTest.addEventListener("click", function() {
-    MopsTest.classList.remove("popup_opened");
+function addCard() {
+    console.log('ДОБАВЛЯЕШЬ КАРТОЧКИ');
+}
+
+function editProfile() {
+    console.log('РЕДАКТИШЬ ПРОФИЛЬ');
+}
+
+popupAll.forEach( (popup) => {
+    popup.addEventListener('click', (event) => {
+        if (event.target.classList.contains('popup__close-button')) {
+            popupClose(popup);
+        }
+    });
+    popup.addEventListener('submit', (event) => {
+        event.preventDefault();
+        return event.target.closest('.popup_card-add') ? addCard() : editProfile();
+    })
 });
 
 
+editButton.addEventListener('click', ()=> popupOpen(popupProfile));
+
+
+  
   
